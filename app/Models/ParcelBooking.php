07fe340ec1e587075626ab7ddfa6bff81738191a
kamrankb <thekamrankb@gmail.com>
2023-08-04 02:59:09 +0500

    Added functions to access through BookingRepository

--- app/Models/ParcelBooking.php
+++ app/Models/ParcelBooking.php
@@ -21,4 +21,28 @@ class ParcelBooking extends Model
         $status = array('Not Booked', 'Booked', 'Picked By Rider', 'Delivered');
         return $status[$value];
     }
+
+    function track($tracking_id) {
+        return static::select('id','pickup','dropoff','rider_id', 'created_by', 'status')
+                        ->where('id', $tracking_id)
+                        ->with('rider:id,first_name');
+    }
+
+    function updateStatus($id, $userID) {
+        return static::find($id)->where('rider_id', $userID);
+    }
+
+    function availableParcel($id="") {
+        $parcel = static::select('id','pickup','dropoff')->whereNull('rider_id');
+
+        if($id!="") {
+            $parcel->where('id', $id);
+        }
+
+        return $parcel;
+    }
+
+    function userBooking($id) {
+        return static::availableParcel()->where('id', $id);
+    }
 }
